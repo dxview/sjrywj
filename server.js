d4a3dd92e655ea14b92b3c246d6849b6974944e8
dxview <dxview93@gmail.com>
2025-12-24 08:18:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "script-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com https://unpkg.com blob:; style-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com; img-src 'self' data: https:;"
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tailwindcss.com https://unpkg.com blob:; style-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com; img-src 'self' data: https:;"
   );
   next();
 });
@@ -67,7 +67,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 // 限流设置
 const submitLimiter = rateLimit({ 
   windowMs: 10 * 60 * 1000, 
